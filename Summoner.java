5ed82ca586fc3b1fe423663f8f5fab6f5e0aa86d
NaijiaoZhang <naijiaozhang489@gmail.com>
2015-11-30 15:18:20 -0500

    Create Summoner.java

---
+++ Summoner.java
@@ -0,0 +1,18 @@
+public class Summoner {
+	private String League,Tier,Division;
+	private int Leaguepoints; 
+	private int[] matchAnaysis = new int[10];
+	
+	
+	
+	public Summoner(String L, String T,String D, int LP){
+		League = L; 
+		Tier = T; 
+		Division = D; 
+		Leaguepoints = LP; 
+	}
+	
+	public void fillAnalysis(String type){
+		
+	}
+}
